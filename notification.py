d4abf8d83dd1b4b582e91b6d8abbf3263e6d5a96
JuManji <opghld@gmail.com>
2022-05-15 17:56:08 +0300

    Create notification.py

---
+++ notification.py
@@ -0,0 +1,33 @@
+from operator import eq
+import subprocess
+import datetime
+import os
+
+print("Start of work")
+bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "accounts:balance"]
+process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
+output, error = process.communicate()
+massive2 = output.split()
+
+massive_of_spend_balance = ((str)(massive2[36])).strip()
+if ((str)(massive_of_spend_balance[2])) == "0":
+    print("Выход! Баланса недостаточно")
+else:
+    print("Выполнение операции депозита")
+    bashCommand = ["yarn", "--cwd", "/root/ironfish/ironfish-cli/", "start", "deposit", "--confirm"]
+    process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
+    string = massive2[36].decode("utf-8")
+    print("Was:")
+    os.system("curl https://api.telegram.org/botID:API/sendMessage?chat_id=ID -d text="+"ABS")
+    print("NOW:")
+    string2 = massive2[28].decode("utf-8")
+
+    e = datetime.datetime.now()
+    print ("Current date and time = %s" % e)
+    print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
+    print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
+
+
+
